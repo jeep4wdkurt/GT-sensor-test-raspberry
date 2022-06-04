2c2c3722f51b0b20ff6715a4283deeb51e0586a4
Kurt Schulte <jeep4wdkurt@gmail.com>
2022-06-04 13:49:48 -0400

    0.0.1 precheck

--- GT-sensor-test-raspberry.py
+++ GT-sensor-test-raspberry.py
@@ -10,7 +10,7 @@
 #    	o xxx Capacitive soil moisture sensor
 #
 # Compatibility:
-#   RaspberryPi-3B
+#   RaspberryPi-3B/1GB
 #
 # Copyright:
 #	Copyright (c) 2022 Kurt Schulte
@@ -49,8 +49,11 @@ def ow_init():
     ow_get_devices()
 
 def ow_get_devices():
-    global ow_device_folders = glob.glob(ow_base_dir + '28*')
-    global ow_device_count = len(ow_device_folders)
+    global ow_device_folders
+    global ow_device_count
+
+    ow_device_folders = glob.glob(ow_base_dir + '28*')
+    ow_device_count = len(ow_device_folders)
 
 def ow_sensor_read_temp_raw(ow_device):
     ow_device_file = ow_device + '/w1_slave'
@@ -72,32 +75,56 @@ def ow_sensor_read_temp(ow_device):
         temp_f = temp_c * 9.0 / 5.0 + 32.0
     return temp_f
 
+def ow_sensor_read_temp_text(ow_device):
+    temp_f = ow_sensor_read_temp(ow_device)
+    device_id_pos = ow_device.find('28-')
+    device_id = ow_device[device_id_pos:]
+    out_text = "Soil Temp @{0} : {1:0.1f} F".format(device_id,temp_f)
+    return out_text
+
 def ow_sensors_read_all():
-    global ow_sensor_temps[]
-    for ow_device in ow_device_folders
-        ow_sensor_read_temp(ow_device)
+    global ow_sensor_temps
+    ow_sensor_temps = []
+    for ow_device in ow_device_folders:
+        temp_text = ow_sensor_read_temp_text(ow_device)
+        ow_sensor_temps.append(temp_text)
+
+def ow_sensors_report_all():
+    global ow_sensor_temps
+    for temp_text in ow_sensor_temps:
+        print(temp_text)
 
 #
 #  DHT11 Routines
 # 
 def dht11_sensor_read():
+    global air_humidity
+    global air_temp_f
     air_humidity, air_temp_c = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,DHT11_PIN)
     air_temp_f = ( air_temp_c * 9.0 / 5.0 ) + 32.0
 
 def dht11_sensor_report():
-    print('Air Temp        : {0:0.1f} F Humidity: {1:0.1f} %'.format(air_humidity))
-    print('Air F Humidity  : {1:0.1f} %'.format(air_humidity))
+    global air_temp_f
+    global air_humidity
+    print('Air Temp                   : {0:0.1f} F'.format(air_temp_f))
+    print('Air Humidity               : {0:0.1f} %'.format(air_humidity))
 
 #
 #  MAIN
 #
 
+print('Growtastic Sensors Test - Raspberry Pi Platform')
+
 ow_init()                   # Initialize OneWire library
 
 while True:
-    
     dht11_sensor_read()
     dht11_sensor_report()
 
+    ow_sensors_read_all()
+    ow_sensors_report_all()
+
     time.sleep(5)
+
+    print ('----------------------------')
 
